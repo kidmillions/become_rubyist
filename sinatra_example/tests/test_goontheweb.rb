bd16576daf1ce32d579f04363b20d3ad7a320df6
Chris <claymanchris@gmail.com>
2015-08-06 19:36:19 -0700

    sinatra-based forms added. wrote server tests

--- sinatra_example/tests/test_goontheweb.rb
+++ sinatra_example/tests/test_goontheweb.rb
@@ -1,11 +1,29 @@
-require "./lib/NAME.rb"
+require "./bin/app.rb"
 require "test/unit"
+require "rack/test"
 
+class SinatraAppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
 
-class TestNAME < Test::Unit::TestCase
+  def app
+    Sinatra::Application
+  end
 
-  def test_sample
-    assert_equal(4, 2+2)
+  def test_my_default
+    get '/'
+    assert_equal 'Hello world', last_response.body
+  end
+
+  def test_hello_form
+    get '/hello-form'
+    assert last_response.ok?
+    assert last_response.body.include?('A greeting')
+  end
+
+  def test_hello_form_post
+    post '/hello-form', params={:name => "Chris", :greeting => "Hi"}
+    assert last_response.ok?
+    assert last_response.body.include?('I just wanted to say')
   end
 
 end
